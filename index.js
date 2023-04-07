37a75d8e047e3d60313ed81bf948d92caf10761f
Ramon A. Garcia <ramon2garcia8@gmail.com>
2023-04-07 19:59:14 -0400

    javascript functions continued

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(someValue = "roller-skate") {
+    return "This Saturday, I want to " + someValue + "!";
+}
+
+saturdayFun();
+
+function mondayWork(someValue = "go to the office") {
+    return "This Monday, I will " + someValue + ".";
+}
+
+mondayWork();
+
+function wrapAdjective(someValue) {
+    return function (innerParameter = "special") {
+        return `You are ${someValue}${innerParameter}${someValue}!`;
+    };
+}
+
+wrapAdjective("%")("a dedicated programmer");
