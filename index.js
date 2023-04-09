bca88f33262cedd2ba66ca189fc30d86b7c0dc83
Ramon A. Garcia <ramon2garcia8@gmail.com>
2023-04-09 10:41:24 -0400

    js functions continued

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // code your solution here
 function saturdayFun(someValue = "roller-skate") {
-    return "This Saturday, I want to " + someValue + "!";
+    return `This Saturday, I want to ${someValue}!`;
 }
 
 saturdayFun();
 
-function mondayWork(someValue = "go to the office") {
-    return "This Monday, I will " + someValue + ".";
+const mondayWork = function (someValue = "go to the office") {
+    return `This Monday, I will ${someValue}.`;
 }
 
 mondayWork();
